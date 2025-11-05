528f6d15f2654d73dbea4cf882751615d635e4bc
df <hello@luenn.xyz>
2025-11-05 16:04:41 +0900

    feat: InventoryLoadException

---
+++ src/main/java/xyz/luenn/framework/exception/InventoryLoadException.java
@@ -0,0 +1,7 @@
+package xyz.luenn.framework.exception;
+
+public class InventoryLoadException extends Exception {
+    public InventoryLoadException(String message) {
+        super(message);
+    }
+}
